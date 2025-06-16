42cc7ea51fe5568a5db80af04947c27166d97eb0
GabrielSougri <gabrielsougri@gmail.com>
2025-06-15 21:39:49 -0300

    correção #5

--- src/prisma/prisma.service.spec.ts
+++ src/prisma/prisma.service.spec.ts
@@ -10,98 +10,9 @@ describe('PrismaService', () => {
     }).compile();
 
     service = module.get<PrismaService>(PrismaService);
-
-   /* await service.$connect()*/
   });
 
-  /* afterAll(async () => {
-    await service.$disconnect()
-  })
-
-  beforeEach(async () => {
-    await service.produtos.deleteMany()
-  })
-  */
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
-  /*
-  // operações de produtos
-  it('esperado criar e recuperar um produto', async () => {
-    const createProduct = await service.produtos.create({
-      data: {
-        nome: 'Quatro queijos',
-        imagem: '',
-        preco: 70.65,
-        descricao: 'feita de quatro queijos',
-        categoriaId: 1
-      }
-    })
-
-    const product = await service.produtos.findUnique({
-      where: {id: createProduct.id}
-    })
-
-    expect(product).toBeDefined()
-    expect(product?.nome).toBe('Quatro queijos')
-  })
-
-  it('esperado retornar um produto', async () => {
-    const createProduct = await service.produtos.create({
-    data: {
-      nome: 'Quatro queijos',
-      imagem: '',
-      preco: 70.65,
-      descricao: 'feita de quatro queijos',
-      categoriaId: 1
-    }
-  })
-
-  const productFound = await service.produtos.findUnique({
-      where: {
-        id: createProduct.id
-      }
-    })
-
-    expect(productFound).toBeDefined()
-    expect(productFound?.nome).toBe('Quatro queijos')
-  })
-
-  it('esperado deletar um produto', async () => {
-    const createProduct = await service.produtos.create({
-      data: {
-        nome: 'Quatro queijos',
-        imagem: '',
-        preco: 70.65,
-        descricao: 'feita de quatro queijos',
-        categoriaId: 1
-      }
-    })
-    const productDeleted = await service.produtos.delete({
-      where: {
-        id: createProduct.id
-      }
-    })
-
-    expect(productDeleted).toBeDefined()
-    expect(productDeleted.nome).toBe('Quatro queijos')
-  })
-
-  // operações de categorias
-  it('esperado retornar a lista de categorias', async () => {
-    const categorias = await service.categorias.findMany()
-    expect(categorias).toBeDefined()
-    expect(categorias.length).toBe(1)
-  })
-
-  it('esperado retornar uma categoria', async () => {
-    const categoriaFound = await service.categorias.findUnique({
-      where: {
-        id: 1
-      }
-    })
-
-    expect(categoriaFound).toBeDefined()
-    expect(categoriaFound?.nome).toBe('Pizzas')
-  }) */
 });
